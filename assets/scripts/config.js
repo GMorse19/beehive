906418d121cccc25af610fef80546d039530ec7a
GMorse19 <malorse@comcast.net>
2019-11-09 18:59:28 -0500

    Added the heroku URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://fathomless-hollows-84563.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
