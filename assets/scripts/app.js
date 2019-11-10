49cfbef881cca0c49d2b9efe49b825a356fa4554
GMorse19 <malorse@comcast.net>
2019-11-09 19:20:45 -0500

    Added auth
    Wrote basic auth functionality.
    
    Works on local host. Now I would like to test the connection with
    heroku.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,14 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
+// const listingEvents = require('./listing/events.js')
+// const rsvpEvents = require('./rsvp/event.js')
 
 $(() => {
   // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
 })
